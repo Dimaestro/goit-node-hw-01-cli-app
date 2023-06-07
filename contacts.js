b9a1b26e0c5a4d10a334477d4152894e8c17178c
Dima <vidistudioproduction@gmail.com>
2023-06-07 16:00:44 +0300

    cli app init project

---
+++ contacts.js
@@ -0,0 +1,25 @@
+
+const contactsPath = null;
+
+function listContacts() {
+  // ...твой код
+}
+
+function getContactById(contactId) {
+  // ...твой код
+}
+
+function removeContact(contactId) {
+  // ...твой код
+}
+
+function addContact(name, email, phone) {
+  // ...твой код
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+}
