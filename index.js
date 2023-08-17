08c4dc35fefec291803be8f345d925e1181f4451
Dima <vidistudioproduction@gmail.com>
2023-08-17 17:32:22 +0300

    finish

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-console.log('Dima Hello');
+const {
+  listContacts,
+  getContactById,
+  addContact,
+  removeContact,
+} = require("./db/contacts.js");
+
+const yargs = require("yargs");
+const { hideBin } = require("yargs/helpers");
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+      const allContacts = await listContacts();
+      console.table(allContacts);
+      break;
+    case "get":
+      const contact = await getContactById(id);
+      console.table(contact);
+      break;
+    case "add":
+      const newContact = await addContact(name, email, phone);
+      console.table(newContact);
+      break;
+    case "remove":
+      const deleteContact = await removeContact(id);
+      console.table(deleteContact);
+      break;
+
+    default:
+      console.warn("\x1B[31m Unknown action type!");
+  }
+};  
+
+const arr = hideBin(process.argv);
+const { argv } = yargs(arr);
+invokeAction(argv);
---
+++ readme.md
@@ -0,0 +1,16 @@
+# CLI app
+
+## Screenshot
+
+
+### List
+[![list](/assets/list.png 'list')](https://monosnap.com/file/pIY6TrkeUlntHUdhIn7uiiOnuuwolF)
+
+### Get
+[![get](/assets/get.png 'get')](https://monosnap.com/file/JPUerIcfbOOzybSxlaVBcMV9MdsK6V)
+
+### Add
+[![add](/assets/add.png 'add')](https://monosnap.com/file/aI2TnH2gSVNYDK97INXGL1YzONN0Tq)
+
+### Remove
+[![remove](/assets/remove.png 'remove')](https://monosnap.com/file/QJDQI63MnX9a3sjyI7S5TRdI6YxYj2)
